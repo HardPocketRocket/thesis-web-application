4b9447fb7edef028226664a4f6f9d9de7591f262
HardPocketRocket <rober3392@gmail.com>
2020-01-11 14:33:29 +0100

    Updated subject schema

--- backend/models/subject.model.js
+++ backend/models/subject.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const subjectSchema = new mongoose.Schema({
     id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, required: true},
     students: [{type : mongoose.Schema.Types.ObjectId, ref: 'Student'}],
     tutors: [{type : mongoose.Schema.Types.ObjectId, ref: 'Tutors'}]
 });
