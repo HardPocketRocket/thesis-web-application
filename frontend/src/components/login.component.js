9cfd6a9579bad2b8ef5619fc5dcec855b4b60de3
HardPocketRocket <rober3392@gmail.com>
2020-02-09 19:04:54 +0100

    WIP UI for login screen

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -1,71 +1,84 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import '../css/login.css'
+
+import { TextField, Button, Typography } from '@material-ui/core';
 
 export default class LoginComponent extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            username: "",
-            password: ""
-        };
+		this.state = {
+			username: '',
+			password: ''
+		};
 
-        //binding the functions this to the class' this
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+		//binding the functions this to the class' this
+		this.onChangeUsername = this.onChangeUsername.bind(this);
+		this.onChangePassword = this.onChangePassword.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
 
-    onChangeUsername(event) {
-        this.setState({
-            username: event.target.value
-        });
-    }
+	onChangeUsername(event) {
+		this.setState({
+			username: event.target.value
+		});
+	}
 
-    onChangePassword(event) {
-        this.setState({
-            password: event.target.value
-        });
-    }
+	onChangePassword(event) {
+		this.setState({
+			password: event.target.value
+		});
+	}
 
-    onSubmit(event) {
-        event.preventDefault();
+	onSubmit(event) {
+		event.preventDefault();
 
-        //User created with password + username to be passed in a request
-        const user = {
-            username: this.state.username,
-            password: this.state.password
-        };
+		//User created with password + username to be passed in a request
+		const user = {
+			username: this.state.username,
+			password: this.state.password
+		};
 
-        axios.post("http://localhost:5000/login", user).then(res => {
-            if(res.status === 200){
-                sessionStorage.setItem("id", res.data)
-                this.props.history.push("/home/" + res.data);
-            }
-        });
-    }
+		axios.post('http://localhost:5000/login', user).then(res => {
+			if (res.status === 200) {
+				sessionStorage.setItem('id', res.data);
+				this.props.history.push('/home/' + res.data);
+			}
+		});
+	}
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <label>Username: </label>
-                    <input
-                        type="text"
-                        value={this.state.username}
-                        onChange={this.onChangeUsername}
-                    />
-                    <br />
-                    <label>Password: </label>
-                    <input
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.onChangePassword}
-                    />
-                    <br />
-                    <input type="submit" value="submit" />
-                </form>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="Centered">
+				<Typography component='h1' variant='h5'>
+					Sign in
+				</Typography>
+				<form className="Form" onSubmit={this.onSubmit}>
+					<TextField
+						className='Input'
+						required
+						autoFocus
+						variant='outlined'
+						label='Username'
+						type='text'
+						value={this.state.username}
+						onChange={this.onChangeUsername}
+					/>
+					<TextField
+						className='Input'
+						required
+						autoFocus
+						variant='outlined'
+						label='Password'
+						type='password'
+						value={this.state.password}
+						onChange={this.onChangePassword}
+					/>
+					<Button className="Button" variant="outlined" type='submit' value='submit'>Sign In</Button>
+				</form>
+			</div>
+		);
+	}
 }
