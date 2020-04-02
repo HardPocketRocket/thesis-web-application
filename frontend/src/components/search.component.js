861afca44cefd6a93034a73a5faee07788bc48f3
HardPocketRocket <rober3392@gmail.com>
2020-04-02 16:35:46 +0200

    Began work on the Ciew Profile Modal

--- frontend/src/components/search.component.js
+++ frontend/src/components/search.component.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import { TextField, Button, withStyles } from '@material-ui/core';
+import { TextField, Button, withStyles, Typography } from '@material-ui/core';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+
+import CloseIcon from '@material-ui/icons/Close';
 
 const styles = {
 	form: {
@@ -56,7 +58,38 @@ const styles = {
 		paddingRight: 16,
 		width: '98%',
 		height: '80%'
-	}
+	},
+	modal: {
+		position: 'fixed',
+		top: 0,
+		left: 0,
+		width: '100%',
+		height: '100%',
+		background: 'rgba(0, 0, 0, 0.6)',
+		display: 'block'
+	},
+	modalMain: {
+		borderRadius: 12,
+		position: 'fixed',
+		background: 'white',
+		width: '50%',
+		height: '40%',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%,-50%)'
+	},
+	modalCloseButton: {
+		position: 'fixed',
+		top: 10,
+		right: 8,
+		height: '8%',
+		width: '8%'
+	},
+	modalText: {
+		marginRight: '30%',
+		marginLeft: '44%',
+		marginTop: '2%'
+	},
 };
 
 class SearchComponent extends Component {
@@ -65,7 +98,8 @@ class SearchComponent extends Component {
 
 		this.state = {
 			query: props.match.params.query,
-			searchResults: []
+			searchResults: [],
+			showViewProfileModal: false
 		};
 
 		this.onSubmit = this.onSubmit.bind(this);
@@ -115,6 +149,14 @@ class SearchComponent extends Component {
 		this.props.history.push('/search/' + this.state.query);
 	}
 
+	showViewProfileModal = () => {
+		this.setState({ showViewProfileModal: true });
+	};
+
+	hideViewProfileModal = () => {
+		this.setState({ showViewProfileModal: false });
+	};
+
 	componentDidUpdate(prevProps) {
 		if (this.props.match.params.query !== prevProps.match.params.query) {
 			this.setState({
@@ -137,6 +179,26 @@ class SearchComponent extends Component {
 	render() {
 		const { classes } = this.props;
 
+		const ViewProfileModal = () => {
+			if (this.state.showViewProfileModal) {
+				return (
+					<div className={classes.modal}>
+						<section className={classes.modalMain}>
+							<Typography
+								className={classes.modalText}
+								variant='h6'>View Profile</Typography>
+							<Button
+								className={classes.modalCloseButton}
+								onClick={this.hideViewProfileModal}
+								startIcon={<CloseIcon />}></Button>
+						</section>
+					</div>
+				);
+			} else {
+				return null;
+			}
+		};
+
 		const Results = props => {
 			if (this.state.searchResults.length < 1) {
 				return null;
@@ -155,7 +217,8 @@ class SearchComponent extends Component {
 				return (
 					<GridListTile
 						key={result._id}
-						onClick={() => this.onUserClicked(result._id)}
+						//onClick={() => this.onUserClicked(result._id)}
+						onClick={() => this.showViewProfileModal()}
 						cols={1}
 						rows={1}>
 						<img
@@ -221,6 +284,8 @@ class SearchComponent extends Component {
 					</Button>
 				</form>
 				<Results results={this.state.searchResults} />
+				<ViewProfileModal
+					show={this.state.showViewProfileModal}></ViewProfileModal>
 			</div>
 		);
 	}
